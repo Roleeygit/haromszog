bd53ec5c5561a40ce2c974d5c59b4feeb6b18b62
Roleeygit <juhaszroli2001@gmail.com>
2022-02-16 10:17:52 +0100

    Kesz

---
+++ App.java
@@ -0,0 +1,27 @@
+/*
+* File: App.java
+* Author: Juhász Roland
+* Copyright: 2022, Juhász Roland
+* Group: Szoft I N 
+* Date: 2022-02-16
+* Github: https://github.com/jani/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+class App {
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.print("alap:");
+      double alap = sc.nextDouble();
+
+
+      System.out.print("Magasság:");
+      double magassag = sc.nextDouble();
+
+      double terulet = (alap * magassag) / 2;
+      System.out.print("Terület:" + terulet);
+    }
+  }
+}
